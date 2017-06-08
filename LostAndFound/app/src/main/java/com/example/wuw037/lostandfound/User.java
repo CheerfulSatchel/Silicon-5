b44e077018965769d16afbe73cdc2c2ce8025580
Dave Gaulton <dbg2pu@virgnia.edu>
2017-06-07 20:24:30 -0400

    User modal

---
+++ LostAndFound/app/src/main/java/com/example/wuw037/lostandfound/User.java
@@ -0,0 +1,23 @@
+package com.example.wuw037.lostandfound;
+
+/**
+ * Created by vff806 on 6/7/17.
+ */
+
+public class User {
+    private String name;
+    private String phoneNum;
+
+    public User(String name, String phoneNum) {
+        this.name = name;
+        this.phoneNum = phoneNum;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getPhone_num() {
+        return this.phoneNum;
+    }
+}
